01ee653837016fc8c5c3d799dbd8192a4c3f3f2e
Mohak Waghchaure <mohak11jan@gmail.com>
2024-05-24 13:25:41 -0400

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,7 +32,7 @@ function Home(){
                             </div>
                         </div> */}
 
-                        <div className='col-lg-4 col-md-4 col-sm-6' data-aos="fade-right" data-aos-easing="ease-in-back" data-aos-delay="200" data-aos-offset="0" >
+                        <div className='col-lg-4 col-md-4' data-aos="fade-right" data-aos-easing="ease-in-back" data-aos-delay="200" data-aos-offset="0" >
                             <div className='picture-wrapper'>
                                 <div className='picture-container'>
                                     <img src={Profile} style={{filter: 'grayscale(0.5)'}}/>
@@ -47,8 +47,13 @@ function Home(){
                                 </div>
                             </div>
                         </div>
-                        <div className='col-lg-8 col-md-8 col-sm-6 col-xs info-wrapper' data-aos="fade-left" data-aos-easing="ease-in-back" data-aos-delay="200" data-aos-offset="0">
+                        <div className='col-lg-8 col-md-8 col-sm-12 col-xs info-wrapper' data-aos="fade-left" data-aos-easing="ease-in-back" data-aos-delay="200" data-aos-offset="0">
                             <div className='text-1'>Hi, I'm Mohak</div>
+                            <div className='sm-screen-picture-wrapper'>
+                                <div className='sm-picture-container'>
+                                    <img src={Profile} style={{filter: 'grayscale(0.5)'}}/>
+                                </div> 
+                            </div>
                             <div className='text-2'>A software developer specialized in <span className='highlight'>Frontend Engineering</span>, Based in New York City</div>
                             <div className='text-4'>
                                 <ul>
@@ -61,7 +66,14 @@ function Home(){
                             <div className='text-3'>Beyond tech I'm proficient in client interactions, successful in team and solo projects, eager to learn new technologies</div>
                             
                             <div className='text-2 highlight'>Let's create something amazing together!</div>
-                            
+                            <div className='sm-buttons-container'>
+                                <div className='btn-wrapper'>
+                                    <button onClick={scrollToContact}>GET IN TOUCH</button>
+                                </div>
+                                <div className='resume-wrap'>
+                                    <a className='resume' href="/portfolio/static/media/Mohak_Sunil_Waghchaure_Resume.pdf">Download Resume</a>
+                                </div>
+                            </div>
                         </div>
                         
                     </div>
