8b4cc51052b2b2470e92d712385eab7ba2e85288
Mohak Waghchaure <mohak11jan@gmail.com>
2024-05-23 21:18:57 -0400

    cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ import ProjectsImage from '../images/icons/projects.png';
 function Projects(){
     var sliderSettings = {
         dots: false,
-        // infinite: true,
+        infinite: true,
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -32,8 +32,8 @@ function Projects(){
                                     <div className='card-wrapper'>
                                         <div className='card'>
                                             <div className='headline'>Talkk.ai</div>
+                                            <div className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
                                             <div className='sub-headline'>Associated with Gray Matrix Pvt Ltd</div>
-                                            <div className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
                                             <div className='btn-wrapper'>
                                                 <button className='btn'>View</button>
                                             </div>
@@ -42,8 +42,28 @@ function Projects(){
                                     <div className='card-wrapper'>
                                         <div className='card'>
                                             <div className='headline'>Differential capital </div>
+                                            <div className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
                                             <div className='sub-headline'>Associated with Gray Matrix Pvt Ltd</div>
-                                            <div className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                                            <div className='btn-wrapper'>
+                                                <button className='btn'>View</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='card-wrapper'>
+                                        <div className='card'>
+                                            <div className='headline'>Playing with Animations</div>
+                                            <div className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
+                                            <div className='sub-headline'></div>
+                                            <div className='btn-wrapper'>
+                                                <button className='btn'>View</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='card-wrapper'>
+                                        <div className='card'>
+                                            <div className='headline'>Playing with Animations</div>
+                                            <div className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </div>
+                                            <div className='sub-headline'></div>
                                             <div className='btn-wrapper'>
                                                 <button className='btn'>View</button>
                                             </div>
